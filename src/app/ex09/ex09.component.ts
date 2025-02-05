eaf9565e870c69d81a20340e7501bd2770b99331
Frigosamu <Frigosamu@gmail.com>
2025-02-05 20:18:57 +0100

    first commit

---
+++ src/app/ex09/ex09.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+
+@Component({
+  selector: 'app-ex09',
+  standalone: true,
+  imports: [
+    NgForOf
+  ],
+  templateUrl: './ex09.component.html'
+})
+export class Ex09Component {
+
+}
