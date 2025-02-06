05c615f66742609452ea9e6c79eee10a090cac4c
Frigosamu <Frigosamu@gmail.com>
2025-02-06 18:04:10 +0100

    Completado, menos el 9

--- src/app/ex08/ex08.component.ts
+++ src/app/ex08/ex08.component.ts
@@ -12,5 +12,26 @@ import {FormsModule} from "@angular/forms";
   templateUrl: './ex08.component.html'
 })
 export class Ex08Component {
+  numbers: number[] = [];
+  fruits: string[] = [];
 
+  num: number = 0;
+  fruit: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.numbers = [1, 5, 8, 24, 32, 11, 55];
+    this.fruits = ["pear", "apple", "mango", "watermelon", "kiwi"];
+  }
+
+  insNumber() {
+    this.numbers.push(this.num);
+    this.num = 0;
+  }
+
+  insFruit() {
+    this.fruits.push(this.fruit);
+    this.fruit = '';
+  }
 }
